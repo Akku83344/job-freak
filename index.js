6e4a78c84bf3e379a72d7a3cb00bb576dabcbc4e
Akku83344 <akku83344@gmail.com>
2023-06-25 22:24:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function showContent(contentId) {
+    var homeContent = document.getElementById('home-content');
+    var contactContent = document.getElementById('contact-content');
+    var infoContent = document.getElementById('info-content');
+    var guideContent = document.getElementById('guide-content');
+  
+    homeContent.style.display = 'none';
+    contactContent.style.display = 'none';
+    infoContent.style.display = 'none';
+    guideContent.style.display = 'none';
+  
+    if (contentId === 'home') {
+      homeContent.style.display = 'block';
+    } else if (contentId === 'contact') {
+      contactContent.style.display = 'block';
+    } else if (contentId === 'info') {
+      infoContent.style.display = 'block';
+    } else if (contentId === 'guide') {
+      guideContent.style.display = 'block';
+    }
+  }
+  
